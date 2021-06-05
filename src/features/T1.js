de2464894602c2c400d2fa67f076119a93829053
str <syosifov@gmail.com>
2021-06-05 17:15:52 +0300

    Login

--- src/features/T1.js
+++ src/features/T1.js
@@ -9,13 +9,31 @@ function T1() {
         console.log(data);
     }
 
-    const getData = function() {
-        fetchData();
+    const fetchCompanies = async () => {
+        const resp = await fetch("https://localhost:8443/api/companies");
+        console.log(resp);
+        const data = await resp.json();
+        console.log(data);
+    }
+
+    const login = async() => {
+        const resp = await fetch("https://localhost:8443/api/v1/auth/login", {
+            method: "POST",
+            body: JSON.stringify({username: "annasmith",
+                                 password: "password"}),
+            headers: {'Accept': 'application/json',
+                      'Content-Type': 'application/json'}
+        });
+        console.log(resp);
+        const data = await resp.json();
+        console.log(data);
     }
 
     return (
         <div>
             <button onClick={fetchData}>Get Posts</button>        
+            <button onClick={fetchCompanies}>Get Companies</button>
+            <button onClick={login}>Login</button>
         </div>
     )
 }
