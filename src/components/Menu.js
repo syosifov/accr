a85058554357fef3b1a29ceb08bd201c2f3abf91
Strashimir Yosifov <syosifov@gmail.com>
2022-02-17 13:04:36 +0200

    Navigation fixes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,8 @@
-import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { authActions, authDataSel } from '../store/AuthSlice'
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions, authDataSel } from "../store/AuthSlice";
 
-import classes from './Menu.module.css';
+import classes from "./Menu.module.css";
 
 function Menu() {
     const dispatch = useDispatch();
@@ -11,20 +11,22 @@ function Menu() {
 
     return (
         <header className={classes.header}>
-            
-                <NavLink
-                    to='/'
-                    className={classes.logo}
-                    // activeClassName={classes.active}
-                >
-                    ACCR
-                </NavLink>
-            
+            <div className={classes.logo}>ACCR</div>
+
             <nav className={classes.nav}>
                 <ul>
                     <li>
+                        <NavLink 
+                            to="/" 
+                            exact
+                            activeClassName={classes.active}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
                         <NavLink
-                            to='/basictable'
+                            to="/basictable"
                             activeClassName={classes.active}
                         >
                             BasicTable
@@ -32,7 +34,7 @@ function Menu() {
                     </li>
                     <li>
                         <NavLink
-                            to='/datatable'
+                            to="/datatable"
                             activeClassName={classes.active}
                         >
                             DataTable
@@ -40,7 +42,7 @@ function Menu() {
                     </li>
                     <li>
                         <NavLink
-                            to='/enhancedtable'
+                            to="/enhancedtable"
                             activeClassName={classes.active}
                         >
                             EnhancedTable
@@ -48,38 +50,42 @@ function Menu() {
                     </li>
                     <li>
                         <NavLink
-                            to='/acctable'
+                            to="/acctable"
                             activeClassName={classes.active}
                         >
                             AccTable
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink
-                            to='/t1'
-                            activeClassName={classes.active}
-                        >
+                        <NavLink to="/t1" activeClassName={classes.active}>
                             t1
                         </NavLink>
                     </li>
-                    {tkn && <li>
-                        <NavLink 
-                            to='/' 
-                            activeClassName={classes.active}
-                            onClick={()=>dispatch(authActions.lgt())}
-                        >
-                            Logout
-                        </NavLink>
-                    </li>}
-                    {!tkn && <li>
-                        <NavLink to='/login' activeClassName={classes.active}>
-                            Login
-                        </NavLink>
-                    </li>}
+                    {tkn && (
+                        <li>
+                            <NavLink
+                                to="/"
+                                activeClassName={classes.active}
+                                onClick={() => dispatch(authActions.lgt())}
+                            >
+                                Logout
+                            </NavLink>
+                        </li>
+                    )}
+                    {!tkn && (
+                        <li>
+                            <NavLink
+                                to="/login"
+                                activeClassName={classes.active}
+                            >
+                                Login
+                            </NavLink>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
 
-export default Menu
+export default Menu;
--- src/mui-test/AccTable.js
+++ src/mui-test/AccTable.js
@@ -6,6 +6,10 @@ const empList = [
     { id: 2, name: "Raj", email: "raj@gmail.com", status: 1, role: 0 },
     { id: 3, name: "David", email: "david342@gmail.com", status: 1, role: 3 },
     { id: 4, name: "Vikas", email: "vikas75@gmail.com", status: 0, role: 2 },
+    { id: 5, name: "Neeraj1", email: "neeraj1@gmail.com", status: 0, role: 1 },
+    { id: 6, name: "Raj1", email: "raj@gmail1.com", status: 1, role: 0 },
+    { id: 7, name: "David1", email: "david3421@gmail.com", status: 1, role: 3 },
+    { id: 8, name: "Vikas1", email: "vikas751@gmail.com", status: 0, role: 2 },
 ];
 
 function AccTable() {
