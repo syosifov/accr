85bc95c0efb7ea58cd6dbbb966449b3970462e93
Strashimir Yosifov <syosifov@gmail.com>
2022-02-17 13:28:15 +0200

    Column alighn, EmptyValue and Lookup

--- src/mui-test/AccTable.js
+++ src/mui-test/AccTable.js
@@ -6,7 +6,7 @@ const empList = [
     { id: 2, name: "Raj", email: "raj@gmail.com", status: 1, role: 0 },
     { id: 3, name: "David", email: "david342@gmail.com", status: 1, role: 3 },
     { id: 4, name: "Vikas", email: "vikas75@gmail.com", status: 0, role: 2 },
-    { id: 5, name: "Neeraj1", email: "neeraj1@gmail.com", status: 0, role: 1 },
+    { id: 5, name: "", email: "neeraj1@gmail.com", status: 0, role: 1},
     { id: 6, name: "Raj1", email: "raj@gmail1.com", status: 1, role: 0 },
     { id: 7, name: "David1", email: "david3421@gmail.com", status: 1, role: 3 },
     { id: 8, name: "Vikas1", email: "vikas751@gmail.com", status: 0, role: 2 },
@@ -16,10 +16,10 @@ function AccTable() {
     const [data, setData] = useState(empList);
     const columns = [
         { title: "ID", field: "id" },
-        { title: "Name", field: "name" },
+        { title: "Name", field: "name", emptyValue: ()=> "Null"  },
         { title: "Email", field: "email" },
-        { title: "Status", field: "status" },
-        { title: "Role", field: "role" },
+        { title: "Status", field: "status", lookup: {0: 'Active', 1: 'On Leave'} },
+        { title: "Role", field: "role", align: "right" },
     ];
     return (
         <>
