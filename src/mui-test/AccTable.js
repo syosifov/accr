dba1e334c951bea5d8c59442903bf32f949cafef
Strashimir Yosifov <syosifov@gmail.com>
2022-02-20 11:53:20 +0200

    Revert "Revert "AccTable rowstyle, selection, cell render""
    This reverts commit 4660d5afda8e613dde7a1e11d4719eb7c360a1ee.

--- src/mui-test/AccTable.js
+++ src/mui-test/AccTable.js
@@ -3,14 +3,15 @@ import MaterialTable from "material-table";
 import GetAppIcon from '@material-ui/icons/GetApp';
 
 const empList = [
-    { id: 1, name: "Neeraj", email: "neeraj@gmail.com", status: 0, role: 1 },
+    { id: 1, name: "Neeraj", email: "neeraj@gmail.com", status: 0, role: 0 },
     { id: 2, name: "Raj", email: "raj@gmail.com", status: 1, role: 0 },
     { id: 3, name: "David", email: "david342@gmail.com", status: 1, role: 3 },
     { id: 4, name: "Vikas", email: "vikas75@gmail.com", status: 0, role: 2 },
     { id: 5, name: "", email: "neeraj1@gmail.com", status: 0, role: 1 },
     { id: 6, name: "Raj1", email: "raj@gmail1.com", status: 1, role: 0 },
     { id: 7, name: "David1", email: "david3421@gmail.com", status: 1, role: 3 },
-    { id: 8, name: "Vikas1", email: "vikas751@gmail.com", status: 0, role: 2 },
+    { id: 8, name: "Vikas1", email: "vikas751@gmail.com", status: 0, role: 2,
+ },
 ];
 
 function AccTable() {
@@ -31,6 +32,17 @@ function AccTable() {
             defaultSort: "asc",
             searchable: false,
             export: false,
+            render: (rowData) => (
+                <div
+                    style={{
+                        background: rowData.role >= 2 ? "#008000aa" : "#f90000aa",
+                        borderRadius: "4px",
+                        paddingLeft: 5,
+                    }}
+                >
+                    {rowData.age >= 2 ? "2+" : "2-"}
+                </div>
+            ),
         },
     ];
     return (
@@ -63,7 +75,18 @@ function AccTable() {
                     exportFileName: "TableData",
                     actionsColumnIndex: -1,
                     addRowPosition: "first",
+                    selection: true,
+                    showSelectAllCheckbox: true,
+                    selectionProps: (rowData) => ({
+                        disabled: rowData.name == null || rowData.name == "",
+                        color: "primary",
+                    }),
+                    grouping: true,
+                    columnsButton: true,
+                    rowStyle: (data, index) =>
+                        index % 3 === 0 ? { background: "#f5f5f5" } : null,
                 }}
+                onSelectionChange={(selectedRows) => console.log(selectedRows)}
                 editable={{
                     onRowAdd: (newRow) =>
                         new Promise((resolve, reject) => {
