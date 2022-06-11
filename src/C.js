406479d1a9cf6b2453382aa630858a2b7ea7a646
Strashimir Yosifov <syosifov@gmail.com>
2022-06-11 12:28:03 +0300

    Get accounts on p_acc

--- src/C.js
+++ src/C.js
@@ -1,9 +1,11 @@
-export const URL        = 'https://localhost:8443';
+export const URL        = 'http://localhost:8000';
+// export const URL        = 'https://localhost:8443';
 // export const URL        = 'http://localhost:8080';
 export const LOGIN      = URL + '/api/v1/auth/login';
 export const REFRESH    = URL + '/api/v1/auth/refresh';
 export const COMPANIES  = URL + '/jpa/companies';
-export const ACCOUNTS   = URL + '/api/v1/accounts';
+// export const ACCOUNTS   = URL + '/api/v1/accounts';
+export const ACCOUNTS   = URL + '/acc/accounts/';
 export const ASSIGN     = URL + '/jpa/v1/assign';
 export const SIGNUP     = URL + '/jpa/v1/signup';
 export const REVERSE    = URL + '/jpa/v1/reverse';
